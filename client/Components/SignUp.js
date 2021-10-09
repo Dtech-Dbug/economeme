3a7ee1a5c8ff7b10b0d1d6874e6372eab07920ca
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 12:02:18 +0530

    imported and used primaryBtn in signUp page

--- client/Components/SignUp.js
+++ client/Components/SignUp.js
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
+import PrimaryBtn from "./UIblocks/PrimaryBtn";
 
 const SignUp = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Sign Up for uploading</h3>
+
+      <PrimaryBtn />
+    </div>
+  );
 };
 
 export default SignUp;
