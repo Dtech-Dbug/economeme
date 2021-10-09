a5dd0f7962c51b57cd88346c60f5923971020795
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 15:21:56 +0530

    update Nav : passed signUp function as props to secondary btn
    - fixed the function

--- client/Components/Nav.js
+++ client/Components/Nav.js
@@ -1,15 +1,28 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 import SecondaryBtn from "./UIblocks/SecondaryBtn";
+import { auth, Provider } from "../firebase";
+import { signInWithPopup } from "@firebase/auth";
+import signup from "../pages/signup";
 
 const Nav = () => {
+  const signUp = async (e) => {
+    e.preventDefault();
+    console.log("jj");
+    console.log(auth);
+    const user = signInWithPopup(auth, Provider);
+    user.then((res) => {
+      console.log("user signed iup-->", res);
+    });
+  };
+
   return (
     <nav className={styles.nav}>
       <h2>SignUp/SignIn to upload your memes</h2>
 
       <ul>
         <li>
-          <SecondaryBtn text="SignUp" />
+          <SecondaryBtn signup={signUp} text="SignUp" />
         </li>
         <li>
           {" "}
