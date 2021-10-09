35e60656a8425dbb3a8fb52cb9a33d2502351ed5
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 12:47:28 +0530

    create secondaryBtn

---
+++ client/Components/UIblocks/SecondaryBtn.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+Xconst SecondaryBtn = () => {
+  return <button className={styles.secondaryBtn}>{text}</button>;
+};
+
+export default SecondaryBtn;
