b63f09555c51e8a2aacc146a4f3b9c6d5dbac0d4
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 21:46:44 +0530

    created controllers for deleting user

--- server/Controllers/userControllers.js
+++ server/Controllers/userControllers.js
@@ -24,7 +24,7 @@ exports.createUser = async (req, res) => {
     res.json(user.rows[0]);
     console.log("user created", user);
   } catch (error) {
-    res.send(400).send("error while creating, view console");
+    res.status(400).send("error while creating, view console");
     console.log(error.message);
   }
 };
@@ -32,4 +32,19 @@ exports.createUser = async (req, res) => {
 exports.getUsers = async (req, res) => {
   const allUsers = await pool.query("SELECT * FROM users");
   res.json(allUsers.rows);
+};
+
+//delete user
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletCategory = await pool.query(`DELETE FROM users WHERE id=$1`, [
+      id,
+    ]);
+
+    res.json(`${id} deleted`);
+  } catch (error) {
+    res.status(400).send("error while creating, view console");
+    console.log("err", error.message);
+  }
 };
