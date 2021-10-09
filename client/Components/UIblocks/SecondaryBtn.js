6cd787d466d0906f3cb05bd334cea9f6e2ba9c18
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 01:55:38 +0530

    wire logout function to btn

--- client/Components/UIblocks/SecondaryBtn.js
+++ client/Components/UIblocks/SecondaryBtn.js
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
+import { useSelector } from "react-redux";
 
-const SecondaryBtn = ({ text, signup }) => {
+const SecondaryBtn = ({ text, signup, logout }) => {
+  const { user } = useSelector((state) => ({ ...state }));
   return (
-    <button onClick={signup} className={styles.secondaryBtn}>
-      {text}
-    </button>
+    <>
+      {user ? (
+        <button onClick={logout} className={styles.secondaryBtn}>
+          {text}
+        </button>
+      ) : (
+        <button onClick={signup} className={styles.secondaryBtn}>
+          {text}
+        </button>
+      )}
+    </>
   );
 };
 
