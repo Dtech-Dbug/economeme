252f497096f7a8adbdb3f5e7e9bec66b8b9c3b48
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 03:21:04 +0530

    styled form component

--- client/Components/UIblocks/Form.js
+++ client/Components/UIblocks/Form.js
@@ -1,16 +1,27 @@
 import React from "react";
+import styles from "../../styles/Home.module.css";
 
 const Form = () => {
   return (
-    <form action="" method="post">
-      <select name="" id="">
-        <option value="">One</option>
-        <option value="">two</option>
+    <form action="" method="post" className={styles.formContainer}>
+      <label htmlFor="Select">
+        Select a category
+        <select name="" id="">
+          <option value="">One</option>
+          <option value="">two</option>
+          <option value="">three</option>
+        </select>
+      </label>
+      <br />
 
-        <option value="">three</option>
-      </select>
-
-      <input type="file" />
+      <label
+        htmlFor="
+      "
+      >
+        {" "}
+        Upload meme
+        <input type="file" />
+      </label>
     </form>
   );
 };
