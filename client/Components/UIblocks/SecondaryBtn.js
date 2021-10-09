6573c5d764cd050460eb12eb880037ad731ce5cc
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 12:48:14 +0530

    fixed : secondarybtn : typos

--- client/Components/UIblocks/SecondaryBtn.js
+++ client/Components/UIblocks/SecondaryBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
 
-Xconst SecondaryBtn = () => {
+const SecondaryBtn = ({ text }) => {
   return <button className={styles.secondaryBtn}>{text}</button>;
 };
 
