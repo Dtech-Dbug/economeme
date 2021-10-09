9aad10d1cb46c34b50fe0b599aba9912cb58b31e
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 19:32:37 +0530

    created : controllers/categoryControllers.js

---
+++ server/Controllers/categoryControllers.js
@@ -0,0 +1,62 @@
+//requiring the connected and confuring database
+const pool = require("../db");
+
+exports.createCategory = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { title } = req.body;
+
+    const addCategory = await pool.query(
+      "INSERT INTO memecategories (title) VALUES($1) RETURNING *",
+      [title]
+    );
+
+    res.json(addCategory);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+exports.getAllCategories = async (req, res) => {
+  try {
+    const fetchAll = "SELECT * FROM memecategories";
+    const allCategories = await pool.query(fetchAll);
+
+    res.json(allCategories.rows);
+  } catch (error) {
+    console.log("Error while fetching all ->", error.message);
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const DELETE = `DELETE FROM memecategories WHERE category_id $1`;
+
+    const deletCategory = await pool.query(
+      `DELETE FROM memecategories WHERE category_id}`,
+      [id]
+    );
+
+    res.json(`${id} deleted`);
+  } catch (error) {
+    console.log("error while deleting:", error.message);
+  }
+};
+
+exports.editCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    // const UPDATE = `UPDATE memecategories SET title = $1 WHERE category_id = $2;`;
+    const update = await pool.query(
+      "UPDATE memecategories SET title = $1 WHERE category_id = $2;",
+      [title, id]
+    );
+
+    res.json(`updated`);
+  } catch (error) {
+    console.log("error while editing:", error.message);
+  }
+};
