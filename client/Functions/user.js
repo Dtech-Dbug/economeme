a0c558079e885173d50ee9e302888a043818f82f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 22:27:42 +0530

    create frontend function to save user to DB

---
+++ client/Functions/user.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const HOST = "http://localhost:8000/api";
+
+export const createUser = async (values) => {
+  return await axios.post(`${HOST}/create-user`, values);
+};
