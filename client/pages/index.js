10da2159d369f0b59d3d416d1ebf846ebfd33cf0
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 12:09:07 +0530

    updated index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
+import SignUp from "../Components/SignUp";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>hhe</div>;
+  return <></>;
 }
