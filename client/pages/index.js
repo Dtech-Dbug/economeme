7d9c37208e4d2fc9814d3320ca224d7d1578ccbb
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 15:21:22 +0530

    update index : removed signUp function from primary btn

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,19 +4,8 @@ import SignUp from "../Components/SignUp";
 import PrimaryBtn from "../Components/UIblocks/PrimaryBtn";
 import SecondaryBtn from "../Components/UIblocks/SecondaryBtn";
 import styles from "../styles/Home.module.css";
-import { auth, Provider } from "../firebase";
-import { signInWithPopup } from "@firebase/auth";
 
 export default function Home() {
-  const signUp = async (e) => {
-    e.preventDefault();
-    console.log("jj");
-    console.log(auth);
-    const user = signInWithPopup(auth, Provider);
-    user.then((res) => {
-      console.log("user signed iup-->", res);
-    });
-  };
   return (
     <div className={styles.homeScreen}>
       <div className={styles.homeScreenContainer}>
@@ -24,7 +13,7 @@ export default function Home() {
 
         <ul>
           <li>
-            <PrimaryBtn handleSignup={signUp} text="Browse 🤡" />
+            <PrimaryBtn text="Browse 🤡" />
           </li>
         </ul>
       </div>
