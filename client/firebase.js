4ec9e89e2e0cbc39a629966fe2d9835aee62f97b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 13:25:23 +0530

    set up firebase SDK

---
+++ client/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import firebase from "firebase";
+import { auth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAkuchgB6A-jWTF0PjpopVmoiDmlpOk2Bo",
+  authDomain: "economeme-519c0.firebaseapp.com",
+  projectId: "economeme-519c0",
+  storageBucket: "economeme-519c0.appspot.com",
+  messagingSenderId: "352789904148",
+  appId: "1:352789904148:web:be10b2304ae01ec91f2001",
+  measurementId: "G-EXQBTH5GJX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = auth();
+export const GoogleAuthProvider = auth.GoogleAuthProvider();
