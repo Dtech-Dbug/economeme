e46e470ff7937c3249b02f07b7ba238a027bd017
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 14:04:03 +0530

    accepted props for handling click

--- client/Components/UIblocks/PrimaryBtn.js
+++ client/Components/UIblocks/PrimaryBtn.js
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
 
-const PrimaryBtn = ({ text }) => {
-  return <button className={styles.primaryBtn}>{text}</button>;
+const PrimaryBtn = ({ text, handleSignup }) => {
+  return (
+    <button onClick={handleSignup} className={styles.primaryBtn}>
+      {text}
+    </button>
+  );
 };
 
 export default PrimaryBtn;
