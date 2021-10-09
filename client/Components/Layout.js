a131e33e486142bb43b20b650dfce71ce78567c6
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 00:56:29 +0530

    used useEffect to fetch current user details and save in redux

--- client/Components/Layout.js
+++ client/Components/Layout.js
@@ -1,7 +1,43 @@
 import styles from "../styles/Home.module.css";
 import Nav from "./Nav";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { onAuthStateChanged } from "@firebase/auth";
+import { auth } from "../firebase";
+import { currentUser } from "../Functions/user";
 
 const Layout = ({ children }) => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth state
+  useEffect(() => {
+    console.log("USEEFFECT");
+    const unSubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userIdToken = await user.getIdTokenResult();
+        console.log("User logged In ", user);
+        console.log("fetching fromoue BE...", userIdToken.token);
+
+        currentUser(userIdToken.token).then((res) => {
+          console.log("res of current", res);
+          dispatch({
+            type: "USER_LOGGED_IN",
+            payload: {
+              email: user.email,
+              token: userIdToken.token,
+              name: user.displayName,
+              id: res.data.id,
+              // name: res.data.name,
+              // id: res.data.id,
+            },
+          });
+        });
+      }
+    });
+    //cleanup
+    return () => unSubscribe();
+  }, []);
+
   return (
     <>
       <Nav />
