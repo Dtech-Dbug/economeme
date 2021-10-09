0e8604cf21dadaf6e36a4c5ca1a95547026cd35a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 18:01:42 +0530

    update : app : wrapped inside provider and passes the store

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,22 @@
 import "../styles/globals.css";
 import Layout from "../Components/Layout";
 
+//setup redux
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "../reducers";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     </>
   );
 }
