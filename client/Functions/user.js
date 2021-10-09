a1d235e0b42ca5993717469ec2d0a88266770e0b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 23:01:41 +0530

    created helper function to interact w/ BE for fetching current user

--- client/Functions/user.js
+++ client/Functions/user.js
@@ -5,3 +5,15 @@ const HOST = "http://localhost:8000/api";
 export const createUser = async (values) => {
   return await axios.post(`${HOST}/create-user`, values);
 };
+
+export const currentUser = async (authtoken) => {
+  return await axios.post(
+    `${HOST}/user`,
+    {},
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+};
