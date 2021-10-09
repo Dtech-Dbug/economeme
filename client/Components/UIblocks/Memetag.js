cbf915743006d490feb8896404f747037a4f362d
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 02:08:30 +0530

    created and styled meme tags

---
+++ client/Components/UIblocks/Memetag.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+const Memetag = () => {
+  return <div className={styles.memetag}>memetag</div>;
+};
+
+export default Memetag;
