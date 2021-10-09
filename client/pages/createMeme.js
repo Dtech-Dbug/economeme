2357a16868b391c96a13c2b3a5c71cce326b767a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 03:40:09 +0530

    updated form component

--- client/pages/createMeme.js
+++ client/pages/createMeme.js
@@ -1,11 +1,41 @@
 import React from "react";
-import Form from "../Components/UIblocks/Form";
+import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
+import { getCategories } from "../Functions/categories";
 
 const createMeme = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((res) => setCategories(res.data));
+  }, []);
   return (
     <>
       <h2 style={{ textAlign: "center" }}>Share the Laughter</h2>
-      <Form />
+      <form action="" method="post" className={styles.formContainer}>
+        <label htmlFor="Select">
+          Select a category
+          <select name="" id="" onClick={(e) => console.log(e.target.value)}>
+            {categories.map((item) => (
+              <>
+                <option key={item.id} value={item.title}>
+                  {item.title}
+                </option>
+              </>
+            ))}
+          </select>
+        </label>
+        <br />
+
+        <label
+          htmlFor="
+      "
+        >
+          {" "}
+          Upload meme
+          <input type="file" />
+        </label>
+      </form>
     </>
   );
 };
