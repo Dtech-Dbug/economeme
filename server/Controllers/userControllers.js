2f1a548345e62ed9b659695e0667f1c6f65d5c59
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 21:29:28 +0530

    update user controller : created controller for getting all users
    - also added error handling

--- server/Controllers/userControllers.js
+++ server/Controllers/userControllers.js
@@ -7,14 +7,29 @@
 const pool = require("../db");
 
 exports.createUser = async (req, res) => {
-  console.log(req.body);
-  const { userName, email } = req.body;
+  try {
+    console.log(req.body);
+    const { userName, email } = req.body;
 
-  const user = await pool.query(
-    "INSERT INTO USERS (username, email) VALUES($1 , $2) RETURNING *",
-    [userName, email]
-  );
+    //for undefined entries skip
+    if (userName === "undefined" || email === "undefined") {
+      return;
+    }
 
-  res.json(user.rows[0]);
-  console.log("user created", user);
+    const user = await pool.query(
+      "INSERT INTO USERS (username, email) VALUES($1 , $2) RETURNING *",
+      [userName, email]
+    );
+
+    res.json(user.rows[0]);
+    console.log("user created", user);
+  } catch (error) {
+    res.send(400).send("error while creating, view console");
+    console.log(error.message);
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  const allUsers = await pool.query("SELECT * FROM users");
+  res.json(allUsers.rows);
 };
