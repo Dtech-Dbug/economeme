9a942035d4b4b0eb22d1898e8949cda853992f43
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 03:05:17 +0530

    CREATE FORM COMPONENT

---
+++ client/Components/UIblocks/Form.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form action="" method="post">
+      <select name="" id="">
+        <option value="">One</option>
+        <option value="">two</option>
+
+        <option value="">three</option>
+      </select>
+
+      <input type="file" />
+    </form>
+  );
+};
+
+export default Form;
