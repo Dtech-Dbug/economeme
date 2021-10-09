93388f27f601f514d744ebce653a156b233fa881
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 03:05:36 +0530

    update createMeme : imported form

--- client/pages/createMeme.js
+++ client/pages/createMeme.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Form from "../Components/UIblocks/Form";
 
 const createMeme = () => {
   return (
     <>
       <h2>react</h2>
+      <Form />
     </>
   );
 };
