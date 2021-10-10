9fe689c437961d385fdd71e556f1a577c99d0f15
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 07:38:48 +0530

    updated feed

--- client/pages/feed.js
+++ client/pages/feed.js
@@ -4,13 +4,20 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { getCategories } from "../Functions/categories";
 import styles from "../styles/Home.module.css";
+import { getMemes } from "../Functions/fileUpload";
+import ImageGrid from "../Components/Card";
+import CARD from "../Components/Card";
 
 const feed = () => {
   const [categories, setCategoroies] = useState([]);
+  const [memes, setMemes] = useState([]);
 
   useEffect(() => {
     //fetch all categories and save in local state
     getCategories().then((res) => setCategoroies(res.data));
+
+    //fetchall meme data
+    getMemes().then((res) => setMemes(res.data));
   }, []);
 
   return (
@@ -21,6 +28,9 @@ const feed = () => {
           return <Memetag memeType={item.title} />;
         })}
       </div>
+      {memes.map((item) => (
+        <CARD src={item.src} category={item.category} />
+      ))}
     </>
   );
 };
