cb8e66efdcf62bfaec53863c82b16f194c6763d9
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-10 07:38:56 +0530

    update : user.js

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -7,13 +7,18 @@ const {
   getUsers,
   deleteUser,
   updateUser,
+  currentUser,
 } = require("../Controllers/userControllers");
+const { authMiddleware } = require("../Middlewares/auth");
 
 //routes
 router.post("/create-user", createUser);
+//current user
+router.post("/user", authMiddleware, currentUser);
+
 router.get("/users", getUsers);
-router.get("/user/:id", getUser);
-router.put("/update/user/:id", updateUser);
+// router.get("/user/:id", getUser);
+// router.put("/update/user/:id", updateUser);
 router.delete("/delete/user/:id", deleteUser);
 
 module.exports = router;
