db063056b497aa16a5b975255d267198afce49af
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-13 21:27:02 +0530

    styled memetags

--- client/Components/UIblocks/Memetag.js
+++ client/Components/UIblocks/Memetag.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "../../styles/Home.module.css";
 
 const Memetag = ({ memeType }) => {
-  return <div className={styles.memetag}>{memeType}</div>;
+  return (
+    <div className={styles.memetag}>
+      <p className={styles.memeTagTextContent}>{memeType}</p>
+    </div>
+  );
 };
 
 export default Memetag;
