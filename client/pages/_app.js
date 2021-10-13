e33ed8fee6c766f6c851455b545a78cc69bcedeb
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-13 21:15:16 +0530

    deleted redundant codes from _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,19 +1,11 @@
 import "../styles/globals.css";
 import Layout from "../Components/Layout";
-import { useEffect } from "react";
-
-import { onAuthStateChanged } from "@firebase/auth";
 
 //setup redux
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "../reducers";
-
-//import function to fetch user details from BE
-import { currentUser } from "../Functions/user";
-import { auth } from "../firebase";
-import { useDispatch } from "react-redux";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
